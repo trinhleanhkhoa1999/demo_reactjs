18f2cc4e4a2635a04e033a9f0e777904e647927c
Khoa Trinh <trinhleanhkhoa1999@gmail.com>
2022-12-10 20:28:57 +0700

    video-16

---
+++ src/components/MyComponent.js
@@ -0,0 +1,10 @@
+// class component
+// function component
+import React, { Component } from "react";
+
+export default class MyComponent extends Component {
+  //JSX code javascript
+  render() {
+    return <div>MyComponent</div>;
+  }
+}
