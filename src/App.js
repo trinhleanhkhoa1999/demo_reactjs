6d9cb17a4cf3eea24359d9bd120227d265dc8939
Khoa Trinh <trinhleanhkhoa1999@gmail.com>
2022-12-20 17:00:56 +0700

    video 47 tao thanh Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.scss";
 
-import React from "react";
+import React, { Component } from "react";
+import Header from "./components/Header/Header";
 
 const App = () => {
   return (
     <div className="app-container">
-      hello world<button>test</button>
+      <Header />
     </div>
   );
 };
