47487adde623b1964329c88af7c788c02540f76b
Khoa Trinh <trinhleanhkhoa1999@gmail.com>
2022-12-20 16:41:48 +0700

    video-46 npm dung version router-dom boostrap

--- src/App.js
+++ src/App.js
@@ -1,37 +1,13 @@
-import logo from "./logo.svg";
 import "./App.scss";
-import { useDispatch, useSelector } from "react-redux";
-import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
-import MyComponent from "./components/MyComponent";
 
-import React, { Component } from "react";
+import React from "react";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        hello world
-        <MyComponent />
-      </div>
-    );
-  }
-}
-
-// const App = () => {
-//   const count = useSelector((state) => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>Hello world</p>
-//         <div>Count = {count}</div>
-//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//       </header>
-//     </div>
-//   );
-// };
+const App = () => {
+  return (
+    <div className="app-container">
+      hello world<button>test</button>
+    </div>
+  );
+};
 
 export default App;
